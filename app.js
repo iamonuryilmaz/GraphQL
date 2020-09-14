d3d51f1d625ad68a524aedcd3430250c930020b7
ONUR YILMAZ <42930346+iamonuryilmaz@users.noreply.github.com>
2020-09-14 09:15:47 +0300

    update

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const expressGraphQL = require('express-graphql');
+const {graphqlHTTP} = require('express-graphql');
 
 const schema = require('./schema/schema');
 
 const app = express();
 
-app.use('/graphql', expressGraphQL =>({
-   schema
+app.use('/graphql', graphqlHTTP({
+   schema,
+   graphiql: true
 }));
 
 app.listen(5000, ()=>{
