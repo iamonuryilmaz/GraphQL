a47a606c9c85a44e69dca53e9483a8b893dd6313
ONUR YILMAZ <42930346+iamonuryilmaz@users.noreply.github.com>
2020-09-14 16:08:28 +0300

    Movie and Director Schema added

--- schema/schema.js
+++ schema/schema.js
@@ -1,47 +1,11 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const movies = [
-    {
-        id: '1',
-        title: 'The Godfather',
-        description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
-        year: 1972,
-        directorId: '1'
-    },
-    {
-        id: '2',
-        title: 'Scarface',
-        description: 'In Miami in 1980, a determined Cuban immigrant takes over a drug cartel and succumbs to greed.',
-        year: 1980,
-        directorId: '1'
-    },
-    {
-        id: '3',
-        title: 'Pulp Fiction',
-        description: 'The lives of two mob hitmen, a boxer, a gangster\'s wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
-        year: 1994,
-        directorId: '1'
-    }
-];
+//Mongo DB Models
 
-const directors = [
-    {
-        id: '1',
-        name: 'Francis Ford Coppola',
-        birth: 1939
-    },
-    {
-        id: '2',
-        name: 'Quentin Tarantino',
-        birth: 1963
-    },
-    {
-        id: '3',
-        name: 'Brian De Palma',
-        birth: 1940
-    }
-];
+const Movie = require('../models/Movie');
+const Director = require('../models/Director');
+
 
 
 const {
