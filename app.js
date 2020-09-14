47d4ca3da27f434bdc329a86ccb5b0d090dc8500
BZC <oktaybzc@gmail.com>
2020-09-14 08:52:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const expressGraphQL = require('express-graphql');
-
-const schema = require('./schema/schema');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 
 const app = express();
 
-app.use('/graphql', expressGraphQL =>({
-   schema
-}));
 
-app.listen(5000, ()=>{
-   console.log("hello world");
+app.use(
+  "/graphql",
+  graphqlHTTP({
+
+  })
+);
+
+app.listen(5000, () => {
+  console.log("hello world");
 });
